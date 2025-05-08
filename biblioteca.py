f600ff0d825b2c3d774e791d1abbfe935709ff9d
raissa soares <raissasoaresandradedasilva@gmail.com>
2025-05-08 14:10:46 -0300

    first commit

---
+++ biblioteca.py
@@ -0,0 +1,11 @@
+class ContaBancaria():
+    def __init__(self, numero, nome, tipo):
+        self.nome = nome
+        self.numero = numero
+        self.tipo = tipo
+
+
+    def Depositar(self):
+    def Sacar (self):
+    def AtivarConta(self):
+    def VerificarSaldo(self):
